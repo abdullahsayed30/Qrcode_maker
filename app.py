de1fc94b938c31b6e4a292c3a5cda285d66b7749
Abdullah <abdullahsayed30@gmail.com>
2022-06-02 13:29:59 +0200

    change background color + add reqirements file

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def make_qr():
 def wifi_window():
     # make the window and its properties
     window2 = Toplevel(main_window)
-    window2.config(bg="#f6f6f7")
+    window2.config(bg="#ffffff")
     window2.grab_set()
     window2.geometry("380x380+{}+{}".format(postion(window2,380,380)[0],postion(window2,380,380)[1]))
     window2.title("QrCode Maker - Share Your Wifi Password")
@@ -55,35 +55,35 @@ def wifi_window():
     window2.iconbitmap("AppIcon.ico")
     
     # Canvas Element to grid the other Elements
-    canvas = tk.Canvas(window2, width=380, height=380, bg="#f6f6f7")
+    canvas = tk.Canvas(window2, width=380, height=380, bg="#ffffff")
     canvas.grid(columnspan=2, rowspan=8)
     
     # Logo
     global logo_icon_s
     logo_icon_s = PhotoImage(file="Images/Logo_S.png")
     logo_icon_s.for_bug = logo_icon_s
-    logo_window2 = Label(window2, image=logo_icon_s, bg="#f6f6f7")
+    logo_window2 = Label(window2, image=logo_icon_s, bg="#ffffff")
     logo_window2.grid(column=0, columnspan=2, row=0)
     
     # Label 1
-    lab1 = Label(window2, text="Wifi Password Share", font=("Comic Sans MS", 13, "bold"), bg="#f6f6f7")
+    lab1 = Label(window2, text="Wifi Password Share", font=("Comic Sans MS", 13, "bold"), bg="#ffffff")
     lab1.grid(column=0, row=1, columnspan=2)
     
     # Water Marks
     # 1         
-    water_mark_window2_1 = Label(window2, image=water_mark_window2_1_photo, bg="#f6f6f7")
+    water_mark_window2_1 = Label(window2, image=water_mark_window2_1_photo, bg="#ffffff")
     water_mark_window2_1.place(x=70, y=101)
     # 2
-    water_mark_window2_2 = Label(window2, image=water_mark_photo_1, bg="#f6f6f7")
+    water_mark_window2_2 = Label(window2, image=water_mark_photo_1, bg="#ffffff")
     water_mark_window2_2.place(x=-40, y=50)
     # 3
-    water_mark_window2_3 = Label(window2, image=water_mark_photo_1, bg="#f6f6f7")    
+    water_mark_window2_3 = Label(window2, image=water_mark_photo_1, bg="#ffffff")    
     water_mark_window2_3.place(x=345, y=270)
     # 4
-    water_mark_window2_4 = Label(window2, image=water_mark_photo_3, bg="#f6f6f7")
+    water_mark_window2_4 = Label(window2, image=water_mark_photo_3, bg="#ffffff")
     water_mark_window2_4.place(x=26, y=169)
     # 5
-    water_mark_window2_5 = Label(window2, image=water_mark_photo_4, bg="#f6f6f7")
+    water_mark_window2_5 = Label(window2, image=water_mark_photo_4, bg="#ffffff")
     water_mark_window2_5.place(x=332, y=238)
 
     # label frame
@@ -91,7 +91,7 @@ def wifi_window():
     labelFrame2.grid(column=0, row=3, padx=5, ipady=5, columnspan=2)
     
     # just to increase the width of labelframe
-    canvas_2 = tk.Canvas(labelFrame2, width=300, height=0, highlightthickness=0, bg="#f6f6f7")
+    canvas_2 = tk.Canvas(labelFrame2, width=300, height=0, highlightthickness=0, bg="#ffffff")
     canvas_2.grid(columnspan=2)
     
     # special var 1
@@ -115,7 +115,7 @@ def wifi_window():
     # Submit Button
     global submit_button
     submit_button = ttk.Button(window2, text="Submit", command=make_wifi_qr, width=25, style="primary.Outline.TButton")
-    s.configure("primary.Outline.TButton", font=("Comic Sans MS", 13), background="#f6f6f7")
+    s.configure("primary.Outline.TButton", font=("Comic Sans MS", 13), background="#ffffff")
     submit_button.grid(column=0, row=6, columnspan=2)
 # -----------End_func_3----------------
 
@@ -212,7 +212,7 @@ s = ttk.Style()
 main_window = S.master
 main_window.title("QrCode Maker")
 main_window.resizable(False, False)
-main_window.config(bg="#f6f6f7")
+main_window.config(bg="#ffffff")
 
 # put the window size and Positions the window in the center of the screen
 main_window.geometry("450x500+{}+{}".format(postion(main_window,450,500)[0],postion(main_window,450,500)[1]))
@@ -222,40 +222,40 @@ main_window.iconbitmap("AppIcon.ico")
 logo_icon_l = PhotoImage(file="Images/Logo_L.png")
 
 # Frame 1
-frame_1 = Frame(main_window, bg="#f6f6f7")
+frame_1 = Frame(main_window, bg="#ffffff")
 frame_1.pack()
 
 # Logo
-logo = Label(frame_1, image=logo_icon_l, bg="#f6f6f7")
+logo = Label(frame_1, image=logo_icon_l, bg="#ffffff")
 logo.grid(column=0, row=0, columnspan=2, pady=15)
 
 # NameOfLogo
-nameOfLogo = Label(frame_1, text="Qrcode Maker", font=("Comic Sans MS", 15, "bold"), bg="#f6f6f7")
+nameOfLogo = Label(frame_1, text="Qrcode Maker", font=("Comic Sans MS", 15, "bold"), bg="#ffffff")
 nameOfLogo.grid(column=0, row=1, columnspan=2, pady=10, sticky="NESW")
 
 # Frame 2
-frame_2 = Frame(main_window, bg="#f6f6f7")
+frame_2 = Frame(main_window, bg="#ffffff")
 frame_2.pack()
 
 # ----------------------WaterMarks---------------------
 # 1
 water_mark_photo_1 = PhotoImage(file="Images/water_mark_1.png")
-water_mark_1 = Label(main_window, image=water_mark_photo_1, bg="#f6f6f7")
+water_mark_1 = Label(main_window, image=water_mark_photo_1, bg="#ffffff")
 water_mark_1.place(x=409, y=390)
 # 2
-water_mark_2 = Label(main_window, image=water_mark_photo_1, bg="#f6f6f7")
+water_mark_2 = Label(main_window, image=water_mark_photo_1, bg="#ffffff")
 water_mark_2.place(x=-40, y=60)
 # 3
 water_mark_photo_2 = PhotoImage(file="Images/water_mark_2.png")
-water_mark_3 = Label(main_window, image=water_mark_photo_2, bg="#f6f6f7")
+water_mark_3 = Label(main_window, image=water_mark_photo_2, bg="#ffffff")
 water_mark_3.place(x=360, y=-22)
 # 4
 water_mark_photo_3 = PhotoImage(file="Images/water_mark_3.png")
-water_mark_4 = Label(main_window, image=water_mark_photo_3, bg="#f6f6f7")
+water_mark_4 = Label(main_window, image=water_mark_photo_3, bg="#ffffff")
 water_mark_4.place(x=-17, y=440)
 # 5
 water_mark_photo_4 = PhotoImage(file="Images/water_mark_4.png")
-water_mark_5 = Label(main_window, image=water_mark_photo_4, bg="#f6f6f7")
+water_mark_5 = Label(main_window, image=water_mark_photo_4, bg="#ffffff")
 water_mark_5.place(x=15, y=410)
 # for window2 Bug Prevention
 water_mark_window2_1_photo = PhotoImage(file="Images/water_mark_5.png")
@@ -263,8 +263,8 @@ water_mark_window2_1_photo = PhotoImage(file="Images/water_mark_5.png")
 
 # LabelFrame
 labelFrame1 = ttk.Labelframe(frame_2, text="Enter Some Text Here:")
-s.configure("TLabelframe.Label", font=("Comic Sans MS", 10), background="#f6f6f7")
-s.configure("TLabelframe", background="#f6f6f7")
+s.configure("TLabelframe.Label", font=("Comic Sans MS", 10), background="#ffffff")
+s.configure("TLabelframe", background="#ffffff")
 labelFrame1.grid(column=0, row=2, padx=8, pady=10, ipady=5)
 
 # TextBox to get the data
@@ -278,11 +278,11 @@ button1.grid(column=0, row=5, columnspan=2, pady=5)
 
 # button to open wifi sharing widow
 button2 = ttk.Button(frame_2, text="Share Your WIFI Password", command=wifi_window, width=30, style="Outline.TButton")
-s.configure("Outline.TButton", font=("Comic Sans MS", 10), background="#f6f6f7")
+s.configure("Outline.TButton", font=("Comic Sans MS", 10), background="#ffffff")
 button2.grid(column=0, row=6, columnspan=2, padx=10, pady=5)
 
 # copyright label
-label2 = Label(frame_2, text="Copyright © 2021 Abdullah Sayed Sallam. All rights reserved", font=("Comic Sans MS", 8), bg="#f6f6f7")
+label2 = Label(frame_2, text="Copyright © 2021 Abdullah Sayed Sallam. All rights reserved", font=("Comic Sans MS", 8), bg="#ffffff")
 label2.grid(column=0, row=7, columnspan=2, pady=7, sticky="NESW")
 
 # to keep the window opened
